44a585f7af7adc20a28723eaa628e7376833e587
Thomas Bosch <tbosch@gmx.de>
2026-02-05 18:33:37 +0100

    tbosch -> codecentric

--- typescript-generator-core/src/main/java/cz/habarta/typescript/generator/TypeScriptGenerator.java
+++ typescript-generator-core/src/main/java/cz/habarta/typescript/generator/TypeScriptGenerator.java
@@ -235,7 +235,7 @@ public class TypeScriptGenerator {
     private static String getVersion() {
         try {
             final InputStream inputStream = TypeScriptGenerator.class.getResourceAsStream(
-                    "/META-INF/maven/de.tbosch.typescript-generator/typescript-generator-core/pom.properties");
+                    "/META-INF/maven/de.codecentric.typescript-generator/typescript-generator-core/pom.properties");
             if (inputStream != null) {
                 final Properties properties = new Properties();
                 properties.load(inputStream);
